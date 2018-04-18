54da9736b8cd2e6c49dcc8d6683745aa7ce5d23f
Hubble <humen.fox@gmail.com>
2018-04-18 17:05:36 +0300

    Added basic template + shared static files

--- main/views.py
+++ main/views.py
@@ -3,5 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Hello, world. You're at the main index.")
-
+	return render(request, 'index.html')
