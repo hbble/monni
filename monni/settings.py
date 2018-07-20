9219a605df95f82500caa47aa24ee15cf7643c02
Andrii Humen <humen.fox@gmai.com>
2018-07-20 13:14:24 +0300

    Disabled mod_wsgi by default, added requirements

--- monni/settings.py
+++ monni/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mod_wsgi.server',
+    #'mod_wsgi.server', #uncomment if you want to use Apache
 ]
 
 MIDDLEWARE = [
