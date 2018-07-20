d74d7a800306c6298b7f8c604786e21db450600b
Andrii Humen <humen.fox@gmai.com>
2018-07-20 14:09:35 +0300

    Added config example file

---
+++ monni/_config.py
@@ -0,0 +1,8 @@
+values = {
+    'SECRET_KEY' : 'paste_your_key_here',
+    'NAME': 'db_name',
+    'USER': 'user_name',
+    'PASSWORD': 'password',
+    'HOST': 'localhost',
+    'PORT': '5432',
+}
